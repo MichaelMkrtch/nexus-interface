29fe2afa510cb6576e2502924119105d0fb11271
Michael <michaelmkrtch@gmail.com>
2026-05-02 10:55:04 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=0, viewport-fit=cover"
+	/>
+</svelte:head>
+
+<div class="app-root bg-black">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<main class="m-10">
+	<div
+		class="flex size-36 items-center justify-center rounded-md border border-neutral-100 font-medium text-white"
+	>
+		Home
+	</div>
+</main>
